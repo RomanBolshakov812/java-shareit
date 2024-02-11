374a5bb5fab8de3a7754c8adfa4d8b7ddcf6c535
Roman Bolshakov <rb812@yandex.ru>
2024-02-11 14:32:01 +0300

    add modules and UserClient

---
+++ server/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -0,0 +1,51 @@
+package ru.practicum.shareit.item;
+
+import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @PostMapping
+    public ItemDto createItem(@Valid @RequestBody ItemDto itemDto,
+                              @RequestHeader("X-Sharer-User-Id") Integer ownerId) {
+        return itemService.addItem(itemDto, ownerId);
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@PathVariable Integer itemId, @RequestBody ItemDto itemDto,
+                              @RequestHeader("X-Sharer-User-Id") Integer ownerId) {
+        return itemService.updateItem(itemId, itemDto, ownerId);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItem(@PathVariable Integer itemId,
+                           @RequestHeader("X-Sharer-User-Id") Integer sharerId) {
+        return itemService.getItem(itemId, sharerId);
+    }
+
+    @GetMapping
+    public List<ItemDto> getItemsByOwnerId(@RequestHeader("X-Sharer-User-Id") Integer ownerId) {
+        return itemService.getItemsByOwnerId(ownerId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String text) {
+        return itemService.searchItem(text);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto createComment(@Valid @RequestBody CommentDto commentDto,
+                                    @PathVariable Integer itemId,
+                                    @RequestHeader("X-Sharer-User-Id") Integer bookerId) {
+        return itemService.addComment(commentDto, itemId, bookerId);
+    }
+}
